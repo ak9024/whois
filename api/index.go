4cef87c23b096d11eb1a292d095558352c0cbf89
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-08-26 10:21:16 +0800

    chore(main): remove return func Handler

--- api/index.go
+++ api/index.go
@@ -16,18 +16,18 @@ type WhoisRequest struct {
 	Domain string `json:"domain"`
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) error {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 
 	var whoisRequest WhoisRequest
 	errDecode := json.NewDecoder(r.Body).Decode(&whoisRequest)
 	if errDecode != nil {
-		return errDecode
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	result, errWhois := whois.Whois(whoisRequest.Domain)
 	if errWhois != nil {
-		return errWhois
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	errEncode := json.NewEncoder(w).Encode(WhoisResponse20xOk{
@@ -35,8 +35,6 @@ func Handler(w http.ResponseWriter, r *http.Request) error {
 		Result: result,
 	})
 	if errEncode != nil {
-		return errEncode
+		w.WriteHeader(http.StatusInternalServerError)
 	}
-
-	return nil
 }
